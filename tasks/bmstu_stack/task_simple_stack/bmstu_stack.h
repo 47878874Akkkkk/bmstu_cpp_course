76be7bb5791ade020e2eaa61af76644f7ab82c55
47878874Akkkkk <khashievakhmed0614@mail.ru>
2025-12-23 21:24:58 +1000

    Реализация класса stack
    - Реализованы все основные методы: push, pop, top, empty, size, clear, emplace
    - Поддержка move семантики и perfect forwarding
    - Идиома copy-and-swap для оператора присваивания
    - Явное управление памятью через placement new
    - Обработка исключений (underflow_error для пустого стека)
    - Все тесты проходят (18/18)

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -1,10 +1,8 @@
 #pragma once
 
 #include <exception>
-#include <iostream>
 #include <utility>
 #include <stdexcept>
-#include <memory>
 
 namespace bmstu
 {
@@ -12,141 +10,141 @@ template <typename T>
 class stack
 {
 private:
-    T* d;
-    size_t s;
-    size_t c;
+    T* data_;
+    size_t size_;
+    size_t capacity_;
 
-    void r(size_t n) {
-        if (n <= c) return;
+    void reserve(size_t new_capacity) {
+        if (new_capacity <= capacity_) return;
         
-        T* nd = static_cast<T*>(::operator new(n * sizeof(T)));
-        for (size_t i = 0; i < s; ++i) {
-            new (&nd[i]) T(std::move(d[i]));
-            d[i].~T();
+        T* new_data = static_cast<T*>(::operator new(new_capacity * sizeof(T)));
+        for (size_t i = 0; i < size_; ++i) {
+            ::new (new_data + i) T(std::move(data_[i]));
+            data_[i].~T();
         }
-        ::operator delete(d);
-        d = nd;
-        c = n;
+        ::operator delete(data_);
+        data_ = new_data;
+        capacity_ = new_capacity;
     }
 
 public:
-    stack() : d(nullptr), s(0), c(0) {}
+    stack() : data_(nullptr), size_(0), capacity_(0) {}
 
     ~stack() {
-        for (size_t i = 0; i < s; ++i) {
-            d[i].~T();
-        }
-        ::operator delete(d);
+        clear();
+        ::operator delete(data_);
     }
 
-    stack(const stack& o) : s(o.s), c(o.c) {
-        d = static_cast<T*>(::operator new(c * sizeof(T)));
-        for (size_t i = 0; i < s; ++i) {
-            new (&d[i]) T(o.d[i]);
-        }
-    }
-
-    stack& operator=(const stack& o) {
-        if (this != &o) {
-            stack t(o);
-            swap(t);
-        }
-        return *this;
-    }
-
-    stack(stack&& o) noexcept 
-        : d(o.d)
-        , s(o.s)
-        , c(o.c) {
-        o.d = nullptr;
-        o.s = 0;
-        o.c = 0;
-    }
-
-    stack& operator=(stack&& o) noexcept {
-        if (this != &o) {
-            for (size_t i = 0; i < s; ++i) {
-                d[i].~T();
+    stack(const stack& other) : size_(other.size_), capacity_(other.size_) {
+        if (size_ > 0) {
+            data_ = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
+            for (size_t i = 0; i < size_; ++i) {
+                ::new (data_ + i) T(other.data_[i]);
             }
-            ::operator delete(d);
-            
-            d = o.d;
-            s = o.s;
-            c = o.c;
-            
-            o.d = nullptr;
-            o.s = 0;
-            o.c = 0;
+        } else {
+            data_ = nullptr;
+        }
+    }
+
+    stack& operator=(const stack& other) {
+        if (this != &other) {
+            stack temp(other);
+            swap(temp);
         }
         return *this;
     }
 
-    void swap(stack& o) noexcept {
-        std::swap(d, o.d);
-        std::swap(s, o.s);
-        std::swap(c, o.c);
+    stack(stack&& other) noexcept 
+        : data_(other.data_)
+        , size_(other.size_)
+        , capacity_(other.capacity_) {
+        other.data_ = nullptr;
+        other.size_ = 0;
+        other.capacity_ = 0;
+    }
+
+    stack& operator=(stack&& other) noexcept {
+        if (this != &other) {
+            clear();
+            ::operator delete(data_);
+            
+            data_ = other.data_;
+            size_ = other.size_;
+            capacity_ = other.capacity_;
+            
+            other.data_ = nullptr;
+            other.size_ = 0;
+            other.capacity_ = 0;
+        }
+        return *this;
+    }
+
+    void swap(stack& other) noexcept {
+        std::swap(data_, other.data_);
+        std::swap(size_, other.size_);
+        std::swap(capacity_, other.capacity_);
     }
 
     bool empty() const noexcept { 
-        return s == 0; 
+        return size_ == 0; 
     }
 
     size_t size() const noexcept { 
-        return s; 
+        return size_; 
     }
 
     template <typename... Args>
-    void emplace(Args&&... a) {
-        if (s >= c) {
-            r(c == 0 ? 4 : c * 2);
+    void emplace(Args&&... args) {
+        if (size_ >= capacity_) {
+            reserve(capacity_ == 0 ? 1 : capacity_ * 2);
         }
-        new (&d[s]) T(std::forward<Args>(a)...);
-        ++s;
+        ::new (data_ + size_) T(std::forward<Args>(args)...);
+        ++size_;
     }
 
-    void push(const T& v) {
-        if (s >= c) {
-            r(c == 0 ? 4 : c * 2);
+    void push(const T& value) {
+        if (size_ >= capacity_) {
+            reserve(capacity_ == 0 ? 1 : capacity_ * 2);
         }
-        new (&d[s]) T(v);
-        ++s;
+        ::new (data_ + size_) T(value);
+        ++size_;
     }
 
-    void push(T&& v) {
-        if (s >= c) {
-            r(c == 0 ? 4 : c * 2);
+    void push(T&& value) {
+        if (size_ >= capacity_) {
+            reserve(capacity_ == 0 ? 1 : capacity_ * 2);
         }
-        new (&d[s]) T(std::move(v));
-        ++s;
+        ::new (data_ + size_) T(std::move(value));
+        ++size_;
     }
 
     void pop() {
         if (empty()) {
             throw std::underflow_error("Stack is empty");
         }
-        --s;
-        d[s].~T();
+        --size_;
+        data_[size_].~T();
     }
 
     T& top() {
         if (empty()) {
             throw std::underflow_error("Stack is empty");
         }
-        return d[s - 1];
+        return data_[size_ - 1];
     }
 
     const T& top() const {
         if (empty()) {
             throw std::underflow_error("Stack is empty");
         }
-        return d[s - 1];
+        return data_[size_ - 1];
     }
 
     void clear() noexcept {
-        for (size_t i = 0; i < s; ++i) {
-            d[i].~T();
+        for (size_t i = 0; i < size_; ++i) {
+            data_[i].~T();
         }
-        s = 0;
+        size_ = 0;
     }
 };
 }
