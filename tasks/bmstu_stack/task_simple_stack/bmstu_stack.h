4adf5930840b1898ca0cd8707311f98d6ff7edcb
47878874Akkkkk <khashievakhmed0614@mail.ru>
2025-12-31 00:24:29 +1000

    Реализация стека: полная рабочая версия

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -11,40 +11,26 @@ class stack
 private:
     T* data_;
     size_t size_;
-    size_t capacity_;
-
-    void reserve(size_t new_capacity) {
-        if (new_capacity <= capacity_) return;
-        
-        T* new_data = static_cast<T*>(::operator new(new_capacity * sizeof(T)));
-        for (size_t i = 0; i < size_; ++i) {
-            ::new (new_data + i) T(std::move(data_[i]));
-            data_[i].~T();
-        }
-        ::operator delete(data_);
-        data_ = new_data;
-        capacity_ = new_capacity;
-    }
 
 public:
-    stack() : data_(nullptr), size_(0), capacity_(0) {}
-
+    stack() : data_(nullptr), size_(0) {}
+    
     ~stack() {
         clear();
-        ::operator delete(data_);
+        operator delete(data_);
     }
-
-    stack(const stack& other) : size_(other.size_), capacity_(other.size_) {
+    
+    stack(const stack& other) : size_(other.size_) {
         if (size_ > 0) {
-            data_ = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
+            data_ = static_cast<T*>(operator new(size_ * sizeof(T)));
             for (size_t i = 0; i < size_; ++i) {
-                ::new (data_ + i) T(other.data_[i]);
+                new (data_ + i) T(other.data_[i]);
             }
         } else {
             data_ = nullptr;
         }
     }
-
+    
     stack& operator=(const stack& other) {
         if (this != &other) {
             stack temp(other);
@@ -52,71 +38,87 @@ public:
         }
         return *this;
     }
-
+    
     stack(stack&& other) noexcept 
         : data_(other.data_)
-        , size_(other.size_)
-        , capacity_(other.capacity_) {
+        , size_(other.size_) {
         other.data_ = nullptr;
         other.size_ = 0;
-        other.capacity_ = 0;
     }
-
+    
     stack& operator=(stack&& other) noexcept {
         if (this != &other) {
             clear();
-            ::operator delete(data_);
+            operator delete(data_);
             
             data_ = other.data_;
             size_ = other.size_;
-            capacity_ = other.capacity_;
             
             other.data_ = nullptr;
             other.size_ = 0;
-            other.capacity_ = 0;
         }
         return *this;
     }
-
+        
     void swap(stack& other) noexcept {
         std::swap(data_, other.data_);
         std::swap(size_, other.size_);
-        std::swap(capacity_, other.capacity_);
     }
-
+    
     bool empty() const noexcept { 
         return size_ == 0; 
     }
-
+    
     size_t size() const noexcept { 
         return size_; 
     }
-
+    
     template <typename... Args>
     void emplace(Args&&... args) {
-        if (size_ >= capacity_) {
-            reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+        T* new_data = static_cast<T*>(operator new((size_ + 1) * sizeof(T)));
+        
+        for (size_t i = 0; i < size_; ++i) {
+            new (new_data + i) T(std::move(data_[i]));
+            data_[i].~T();
         }
-        ::new (data_ + size_) T(std::forward<Args>(args)...);
+        
+        new (new_data + size_) T(std::forward<Args>(args)...);
+        
+        operator delete(data_);
+        data_ = new_data;
         ++size_;
     }
-
+    
     void push(const T& value) {
-        if (size_ >= capacity_) {
-            reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+        T* new_data = static_cast<T*>(operator new((size_ + 1) * sizeof(T)));
+        
+        for (size_t i = 0; i < size_; ++i) {
+            new (new_data + i) T(std::move(data_[i]));
+            data_[i].~T();
         }
-        ::new (data_ + size_) T(value);
+        
+        new (new_data + size_) T(value);
+        
+        operator delete(data_);
+        data_ = new_data;
         ++size_;
     }
-
+    
     void push(T&& value) {
-        if (size_ >= capacity_) {
-            reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+        T* new_data = static_cast<T*>(operator new((size_ + 1) * sizeof(T)));
+        
+        for (size_t i = 0; i < size_; ++i) {
+            new (new_data + i) T(std::move(data_[i]));
+            data_[i].~T();
         }
-        ::new (data_ + size_) T(std::move(value));
+        
+        new (new_data + size_) T(std::move(value));
+        
+        operator delete(data_);
+        data_ = new_data;
         ++size_;
     }
-
+    
     void pop() {
         if (empty()) {
             throw std::underflow_error("Stack is empty");
@@ -124,21 +126,21 @@ public:
         --size_;
         data_[size_].~T();
     }
-
+    
     T& top() {
         if (empty()) {
             throw std::underflow_error("Stack is empty");
         }
         return data_[size_ - 1];
     }
-
+    
     const T& top() const {
         if (empty()) {
             throw std::underflow_error("Stack is empty");
         }
         return data_[size_ - 1];
     }
-
+    
     void clear() noexcept {
         for (size_t i = 0; i < size_; ++i) {
             data_[i].~T();
@@ -146,4 +148,4 @@ public:
         size_ = 0;
     }
 };
-}  // namespace bmstu
+}
